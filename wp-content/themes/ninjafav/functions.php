f2d041372d74cb17309adf828281b33f8ed2f38a
Tanner Moushey <tanner@iwitnessdesign.com>
2018-02-12 15:49:49 -0800

    Update EDD Slug

--- wp-content/themes/ninjafav/functions.php
+++ wp-content/themes/ninjafav/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+// customize the EDD Slug
+define('EDD_SLUG', 'ninjas');
+
+/**
+ * Enqueue default stylesheet
+ *
+ * @since  1.0.0
+ *
+ * @author Tanner Moushey
+ */
 function ninjafav_stylesheet() {
 	wp_dequeue_style( 'checkout-style' );
 	wp_enqueue_style( 'checkout-style', get_template_directory_uri() . '/style.css' );
